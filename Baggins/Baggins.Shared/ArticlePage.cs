5851da05e6ea5a9977cddb8d5b8cbc2aa5c95b28
Bijoy Singh <bijoysingh693@gmail.com>
2015-01-10 22:57:18 +0530

    initial commit

--- Baggins/Baggins.Shared/ArticlePage.cs
+++ Baggins/Baggins.Shared/ArticlePage.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
 namespace Baggins
 {
-    public partial class ArticlePage
+    public partial class ArticlePage : Page
     {
 
           public ArticlePage()
