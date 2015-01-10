d00a40b133b77cc52beb444f43852ca6d51e938b
Bijoy Singh <bijoysingh693@gmail.com>
2015-01-10 22:48:43 +0530

    Adding items

---
+++ Baggins/Baggins.Shared/ArticlePage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baggins
+{
+    public partial class ArticlePage
+    {
+
+          public ArticlePage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+    }
+}
