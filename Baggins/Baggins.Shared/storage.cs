7e48e7308c6107d844109ad11d2cbe2078c6d8ca
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2015-01-12 10:37:22 +0530

    Some commit

---
+++ Baggins/Baggins.Shared/storage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure;
+
+
+namespace Baggins
+{
+    class storage
+    {
+        String connectionString = "DefaultEndpointsProtocol=https;AccountName=baggins;AccountKey=1paj4H4YkZqeHEkS1/L9pZpVYFP0BWKp/Dpk1brvmlw4ZJu2thEipVM/0QLix5GQuidYlI0jofkm5qRJsTiMwA==";
+        CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+        
+        
+    }
+}
